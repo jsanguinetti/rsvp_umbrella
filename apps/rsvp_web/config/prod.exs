862cd0ed4917a2b06a31a0c8dc7d1e7df72855cb
jsanguinetti <jsangui@gmail.com>
2018-04-21 15:05:15 -0300

    try fixing sockets on heroku

--- apps/rsvp_web/config/prod.exs
+++ apps/rsvp_web/config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rsvp_web, RsvpWeb.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "phoenix-test-app-events-123.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
